116016954523984f0fa07451d5bf6d678d779b1f
John Cleve <johnpcleve@gmail.com>
2022-09-01 10:29:04 -0400

    Update truffle-cleaner.py

--- truffle-cleaner.py
+++ truffle-cleaner.py
@@ -1,5 +1,8 @@
 import json
+import yaml
 import argparse
+from os.path import exists
+from os.path import basename
 
 def main():
 
@@ -7,11 +10,24 @@ def main():
     parser = argparse.ArgumentParser(description='Scrub Trufflehog results')
     parser.add_argument('--file', type=str, required=True)
     parser.add_argument('--fail', default=False, action="store_true")
+    parser.add_argument('--exclude', type=str)
     args = parser.parse_args()
 
+    #  vars
     parsed = []
+    excluded = []
     pre_parsed = 0
     post_parsed = 0
+
+    # load allow list
+    if args.exclude is not None and exists(args.exclude):
+        with open(args.exclude, 'r') as stream:
+            exclusions = yaml.safe_load(stream)
+            for file in exclusions:
+                excluded.append(file)
+        print('Excluded files: ' + str(excluded))
+    else:
+        print('No exclusion file passed...\n')
 
     # process trufflehog findings
     with open(args.file) as file:
@@ -23,9 +39,12 @@ def main():
             pre_parsed+=1
             if data['Redacted'] == "":
                 continue
+            # ignore files added to exclude/allow list
+            if basename(data['SourceMetadata']['Data']['Filesystem']['file']) in excluded:
+                continue
             else:
                 post_parsed+=1
-                parsed.append(data) 
+                parsed.append(data)
                 print(line + '\n')
 
     # print basic findings
@@ -42,5 +61,8 @@ def main():
     if post_parsed > 0 and args.fail:
         raise RuntimeError('Exposed secrets detected')
 
+
 if __name__ == "__main__":
     main()
+
+
